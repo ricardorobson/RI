d48aff9be72f69dade5c164bcb78a26e58eb2ba4
Deyvson Lazaro <dls4@cin.ufpe.br>
2018-07-03 14:45:45 -0300

    Web search engine initial commit

---
+++ wse/src/main/java/com/ri/wse/WseApplication.java
@@ -0,0 +1,12 @@
+package com.ri.wse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WseApplication.class, args);
+    }
+}
